967afa30fd233c6ce4712c9dc99e0b8ec041761b
John Wiegley <johnw@newartisans.com>
2017-06-20 15:29:35 -0700

    Update example to pass state

--- Library.hs
+++ Library.hs
@@ -2,16 +2,29 @@
 
 module Library where
 
+import Foreign.Ptr
+import Foreign.StablePtr
 import Foreign.C.Types
 import Foreign.C.String
+import Foreign.Storable
 import System.IO.Unsafe
 
-foreign export ccall c_sampleMethod :: CInt -> CString
+foreign export ccall c_initState :: CInt -> Ptr ()
+foreign export ccall c_sampleMethod :: CInt -> Ptr () -> Ptr (Ptr ()) -> CString
 
-c_sampleMethod :: CInt -> CString
-c_sampleMethod n = unsafePerformIO $ do
-    let s = sampleMethod (fromIntegral n)
-    newCString s
+c_initState :: CInt -> Ptr ()
+c_initState n = castStablePtrToPtr $ unsafePerformIO $ newStablePtr n
 
-sampleMethod :: Int -> String
-sampleMethod = show
+c_sampleMethod :: CInt -> Ptr () -> Ptr (Ptr ()) -> CString
+c_sampleMethod n st res = unsafePerformIO $ do
+    let v = castPtrToStablePtr st :: StablePtr Int
+    st' <- deRefStablePtr v
+    let (str, n') = sampleMethod (fromIntegral n) st'
+    str' <- newCString str
+    freeStablePtr v
+    v' <- newStablePtr n'
+    poke res (castStablePtrToPtr v')
+    return str'
+
+sampleMethod :: Int -> Int -> (String, Int)
+sampleMethod x y = (show x ++ ":" ++ show y, y + 1)
--- test/main.c
+++ test/main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include "HsFFI.h"
 
-extern char *c_sampleMethod(int);
+extern void *c_initState(int);
+extern char *c_sampleMethod(int, void *, void **);
 
 int main(int argc, char *argv[]) {
   hs_init(&argc, &argv);
 
-  char *result = c_sampleMethod(100);
+  void *state = c_initState(50);
+
+  char *result = c_sampleMethod(100, state, &state);
+  printf("Result = %s\n", result);
+
+  result = c_sampleMethod(200, state, &state);
+  printf("Result = %s\n", result);
+
+  result = c_sampleMethod(300, state, &state);
   printf("Result = %s\n", result);
 
   hs_exit();
